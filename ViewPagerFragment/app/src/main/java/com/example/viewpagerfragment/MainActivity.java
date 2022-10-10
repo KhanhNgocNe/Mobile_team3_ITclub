67221b0290fac321e5ee087849fc5a9d4e34856d
KhanhNgocNe <ngocdkgcc19167@fpt.edu.vn>
2022-10-10 15:44:30 +0700

    update, note done - listview in fragment is error

---
+++ ViewPagerFragment/app/src/main/java/com/example/viewpagerfragment/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.viewpagerfragment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPage;
+    private BottomNavigationView mbottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPage = findViewById(R.id.view_paper);
+        mbottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewPaperAdapter  adapter = new ViewPaperAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPage.setAdapter(adapter);
+
+        mViewPage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        mbottomNavigationView.getMenu().findItem(R.id.home1).setChecked(true);
+                        break;
+                    case 1:
+                        mbottomNavigationView.getMenu().findItem(R.id.favorite1).setChecked(true);
+                        break;
+                    case 2:
+                        mbottomNavigationView.getMenu().findItem(R.id.user1 ).setChecked(true);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        mbottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home1:
+                        mViewPage.setCurrentItem(0);
+                        break;
+                    case R.id.favorite1:
+                        mViewPage.setCurrentItem(1);
+                        break;
+                    case R.id.user1:
+                        mViewPage.setCurrentItem(2);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+}
